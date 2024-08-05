42b679a2a02dfcafcea4ad502c31fca21014d103
Marco Orellana <mafeorellana09@gmail.com>
2024-08-04 23:54:00 -0400

    first commit

--- src/components/userspage/IndexPage.jsx
+++ src/components/userspage/IndexPage.jsx
@@ -46,11 +46,6 @@ const IndexPage = () => {
 
 
 
-
-
-
-
-
                     </div>
                 </div>
             </div>
--- src/components/userspage/IndexPage2.jsx
+++ src/components/userspage/IndexPage2.jsx
@@ -16,25 +16,8 @@ const PaginaNumeroDos = () => {
                 </div>
             </div>
 
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col">
-                        <h1 className='text-white'>Bienvenido a la Nueva Página</h1>
-                        <p className='text-white'>Esta es una plantilla básica para una nueva página en React.</p>
-                        <Link to="/" className="btn btn-danger">Volver a la Página Principal haz click aqui</Link>
-                    </div>
-                </div>
-            </div>
+           
 
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col">
-                        <h1 className='text-white'>Bienvenido a la Nueva Página</h1>
-                        <p className='text-white'>Esta es una plantilla básica para una nueva página en React.</p>
-                        <Link to="/" className="btn btn-danger">Volver a la Página Principal haz click aqui</Link>
-                    </div>
-                </div>
-            </div>
             
         </div>
     );
